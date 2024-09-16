338ed2b32e02cb1516291b700deeefa3ac887908
8u3631984 <8u3631984@gmail.com>
2024-09-16 20:24:46 +0200

    add roup configuration

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -59,7 +59,33 @@ groups = [
     Group("8", layout='monadtall'),
     Group("9", layout='monadtall'),
 ]
-dgroups_key_binder = simple_key_binder(mod)
+#dgroups_key_binder = simple_key_binder(mod)
+
+
+for i in groups:
+    keys.extend(
+        [
+            # mod + group number = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod + shift + group number = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod + shift + group number = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
 
 # --------------------------------------------------------
 # Layouts
@@ -90,6 +116,10 @@ widget_defaults = dict(
     fontsize=14,
     padding=3
 )
+
+widget_list = [
+    
+]
 extension_defaults = widget_defaults.copy()
 
 screens = [
