3f539fb4a269ecb7025313b51fc1854162c87dc5
8u3631984 <8u3631984@gmail.com>
2024-09-17 08:59:48 +0200

    update config

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -1,3 +1,15 @@
+
+#    ____  __  _ __                 ______            _____                        __  _           
+#   / __ \/ /_(_) /__              / ____/___  ____  / __(_)___ ___  ___________ _/ /_(_)___  ____ 
+#  / / / / __/ / / _ \   ______   / /   / __ \/ __ \/ /_/ / __ `/ / / / ___/ __ `/ __/ / __ \/ __ \
+# / /_/ / /_/ / /  __/  /_____/  / /___/ /_/ / / / / __/ / /_/ / /_/ / /  / /_/ / /_/ / /_/ / / / /
+# \___\_\__/_/_/\___/            \____/\____/_/ /_/_/ /_/\__, /\__,_/_/   \__,_/\__/_/\____/_/ /_/ 
+#                                                      /____/                                     
+
+# --------------------------------------------------------
+# see https://docs.qtile.org/
+# --------------------------------------------------------
+
 from libqtile import bar, layout, qtile, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
@@ -8,59 +20,73 @@ import json
 # --------------------------------------------------------
 # General Variables
 # --------------------------------------------------------
-mod         = "mod4"
+mod         = "mod4"        # windows key
 terminal    = "alacritty"
 
-# --------------------------------------------------------
-# Keybindings
-# --------------------------------------------------------
+
 keys = [
-    # Focus
-    Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-    #Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window around"),
-    
-    # Move
-    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
-
-    # Size
-    Key([mod, "control"], "Down", lazy.layout.shrink(), desc="Grow window to the left"),
-    Key([mod, "control"], "Up", lazy.layout.grow(), desc="Grow window to the right"),
-
-    # Floating
-    Key([mod], "t", lazy.window.toggle_floating(), desc='Toggle floating'),
-    
-    # Toggle Layouts
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-
-    #System
-    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
-    
-    # Apps
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key(
+        [mod],
+        "f",
+        lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen on the focused window",
+    ),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
 
-# --------------------------------------------------------
-# Groups
-# --------------------------------------------------------
-groups = [
-    Group("1", layout='monadtall'),
-    Group("2", layout='monadtall'),
-    Group("3", layout='monadtall'),
-    Group("4", layout='monadtall'),
-    Group("6", layout='monadtall'),
-    Group("7", layout='monadtall'),
-    Group("8", layout='monadtall'),
-    Group("9", layout='monadtall'),
-]
-#dgroups_key_binder = simple_key_binder(mod)
+# Add key bindings to switch VTs in Wayland.
+# We can't check qtile.core.name in default config as it is loaded before qtile is started
+# We therefore defer the check until the key binding is run by using .when(func=...)
+for vt in range(1, 8):
+    keys.append(
+        Key(
+            ["control", "mod1"],
+            f"f{vt}",
+            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+            desc=f"Switch to VT{vt}",
+        )
+    )
 
+
+groups = [Group(i) for i in "123456789"]
 
 for i in groups:
     keys.extend(
@@ -86,45 +112,32 @@ for i in groups:
         ]
     )
 
-
-# --------------------------------------------------------
-# Layouts
-# --------------------------------------------------------
-layout_theme = { 
-    "border_width": 2,
-    "margin": 5,
-    "border_focus": "#FF0000",
-    "border_normal": "#FFFFFF",
-    "single_border_width": 2
-}
-
-layouts = [    
-    layout.MonadTall(**layout_theme),
-    layout.MonadWide(**layout_theme),
-    layout.Tile(**layout_theme),
-    layout.Columns(**layout_theme),
-    layout.Floating()
+layouts = [
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
 ]
-
-
-# --------------------------------------------------------
-# Setup Widget Defaults
-# --------------------------------------------------------
 
 widget_defaults = dict(
-    font="JetBrains Mono Light",
-    fontsize=14,
-    padding=3
+    font="sans",
+    fontsize=12,
+    padding=3,
 )
-
-widget_list = [
-    
-]
 extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        top=bar.Bar(
+        bottom=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
@@ -136,25 +149,12 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
+                widget.TextBox("default config", name="default"),
+                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
                 widget.Systray(),
-                
-                widget.Memory(                    
-                    background="#ffffff"+".4",
-                    padding=10,        
-                    measure_mem='G',
-                    format="{MemUsed:.0f}{mm} ({MemTotal:.0f}{mm})"
-                ),
-                
-                widget.Volume(                    
-                    background="#ffffff"+".4",
-                    padding=10, 
-                    fmt='Vol: {}',
-                ),
-                
                 widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-
                 widget.QuickExit(),
             ],
             24,
@@ -167,8 +167,6 @@ screens = [
         # x11_drag_polling_rate = 60,
     ),
 ]
-
-
 
 # Drag floating layouts.
 mouse = [
@@ -210,5 +208,12 @@ wl_input_rules = None
 wl_xcursor_theme = None
 wl_xcursor_size = 24
 
-
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
 wmname = "LG3D"
