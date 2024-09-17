67c9bd49bc237ecd9ae4d0cbbc3445a6c416f528
8u3631984 <8u3631984@gmail.com>
2024-09-17 16:25:49 +0200

    add bar 1

--- config/qtile/bar1.py
+++ config/qtile/bar1.py
@@ -1,4 +1,4 @@
-from libqtile import bar
+from libqtile.bar import Bar
 from libqtile.widget.battery import Battery
 from libqtile.widget.clock import Clock
 from libqtile.widget.cpu import CPU
@@ -12,35 +12,77 @@ from libqtile.widget.window_count import WindowCount
 from libqtile.widget.windowname import WindowName
 
 
-BAR_HEIGHT = 28
+bar = Bar([
+    GroupBox(
+        disable_drag=True,
+        #active=nord_fox['magenta'],
+        #inactive=nord_fox['black'],
+        highlight_method='line',
+        #block_highlight_text_color=nord_fox['fg_gutter'],
+        borderwidth=0,
+        #highlight_color=nord_fox['bg'],
+        #background=nord_fox['bg'],
+        # spacing=2
+    ),
+    #left_half_circle(nord_fox['red'], nord_fox['bg']),
+    CurrentLayout(
+    #    background=nord_fox['red'],
+    #    foreground=nord_fox['white'],
+        margin=10,
+    ),
+
+    #right_arrow(nord_fox['fg_gutter'], nord_fox['red']),
+    WindowCount(
+        text_format='缾 {num}',
+    #    background=nord_fox['fg_gutter'],
+    #    foreground=nord_fox['white'],
+        show_zero=True,
+    ),
+    #right_half_circle(nord_fox['fg_gutter'], nord_fox['bg']),
+
+    WindowName(
+    #    background=nord_fox['bg'],
+    #    foreground=nord_fox['fg']
+    ),
+
+    #left_half_circle(nord_fox['black'], nord_fox['bg']),
+    CPU(
+        format=' {freq_current}GHz {load_percent}%',
+    #    background=nord_fox['black'],
+    #    foreground=nord_fox['pink']
+    ),
+
+    Memory(
+        format=' {MemUsed: .0f}{mm}/{MemTotal: .0f}{mm}',
+    #    background=nord_fox['black'],
+    #    foreground=nord_fox['cyan']
+    ),
+
+    Net(
+    #    background=nord_fox['black'],
+    #    foreground=nord_fox['green']
+    ),
+    # # Battery(
+    # #     background=gruvbox['fg3'],
+    # #     format='{char} {percent:2.0%} {hour:d}:{min:02d}'
+    # # ),
+
+    #left_half_circle(nord_fox['fg_gutter'], nord_fox['black']),
+    Systray(
+    #    background=nord_fox['fg_gutter']
+    ),
+    #right_half_circle(nord_fox['fg_gutter'], nord_fox['black']),
+
+    Clock(
+    #    background=nord_fox['black'],
+    #    foreground=nord_fox['white'],
+        format=' %Y-%m-%d %a %I:%M %p'
+    ),
 
 
-bar=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
 
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                widget.Systray(),
-                #widget.CheckUpdates(),
-                widget.Clock(format="%a %d.%m.%Y %I:%M %p"),
-                widget.QuickExit(),
-            ],
-            24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-        ),
-        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-        # By default we handle these events delayed to already improve performance, however your system might still be struggling
-        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
-    
+],
+    # background=nord_fox['bg'],
+    size=28,
+    margin=8,
+)
