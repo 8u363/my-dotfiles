21b33aac8888cd15b457085e433518507227fc8c
8u3631984 <8u3631984@gmail.com>
2024-09-17 20:13:44 +0200

    move to top

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -161,7 +161,7 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
