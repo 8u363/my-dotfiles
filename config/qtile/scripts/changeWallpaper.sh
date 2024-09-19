756bee85a260e3b7ba583ae594d825fb807b4aaf
8u3631984 <8u3631984@gmail.com>
2024-09-19 20:47:35 +0200

    reset wallpaper

--- config/qtile/scripts/changeWallpaper.sh
+++ config/qtile/scripts/changeWallpaper.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 feh --bg-scale --randomize ~/git/my-wallpaper/art
+
+  wallpaper="$(cat "${HOME}/.fehbg" | awk -F "'" '{print $2}')"
+
+  # Apply pywal color scheme to desktop
+  wal -i $wallpaper
