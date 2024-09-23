e39eeda49242137b4e376a1180c9d859df511ec4
8u3631984 <8u3631984@gmail.com>
2024-09-23 09:45:32 +0200

    update scratchpad psoitions

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -107,9 +107,9 @@ for i in groups:
 # Scratchpads
 # --------------------------------------------------------
 groups.append(ScratchPad("6", [        
-    DropDown("btop", "alacritty -e btop", x=0.3, y=0.1, width=0.40, height=0.4, on_focus_lost_hide=False ),    
-    DropDown("thunar", "thunar", x=0.3, y=0.1, width=0.40, height=0.4, on_focus_lost_hide=False ),   
-    DropDown("nitrogen", "nitrogen", x=0.3, y=0.1, width=0.40, height=0.4, on_focus_lost_hide=False ),     
+    DropDown("btop", "alacritty -e btop", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),    
+    DropDown("thunar", "thunar", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),   
+    DropDown("nitrogen", "nitrogen", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),     
     DropDown("terminal", "alacritty", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False ),
 ]))
 
