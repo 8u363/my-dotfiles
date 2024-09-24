5e57151b5da035cf8c3c32695158802e704cddee
8u3631984 <8u3631984@gmail.com>
2024-09-24 22:01:44 +0200

    config groups

--- config/qtile/configGroups.py
+++ config/qtile/configGroups.py
@@ -12,40 +12,42 @@ from  globalVariables import *
 # We can't check qtile.core.name in default config as it is loaded before qtile is started
 # We therefore defer the check until the key binding is run by using .when(func=...)
 def initGroups():
-    for i in range(1, numerOfGroups):
-        groups.append(Group(i))
-        
+    for vt in range(1, 6):
         keys.append(
             Key(
                 ["control", "mod1"],
-                f"f{i}",
-                lazy.core.change_vt(i).when(func=lambda: qtile.core.name == "wayland"),
-                desc=f"Switch to VT{i}",
+                f"f{vt}",
+                lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+                desc=f"Switch to VT{vt}",
             )
         )
-               
+
+
+    groups = [Group(i) for i in "123456"]
+
+    for i in groups:
         keys.extend(
             [
                 # mod + group number = switch to group
                 Key(
                     [mod],
-                    str(i),
-                    lazy.group[str(i)].toscreen(),
-                    desc="Switch to group {}".format(str(i)),
+                    i.name,
+                    lazy.group[i.name].toscreen(),
+                    desc="Switch to group {}".format(i.name),
                 ),
                 # mod + shift + group number = switch to & move focused window to group
                 Key(
                     [mod, "shift"],
-                    str(i),
-                    lazy.window.togroup(str(i), switch_group=True),
-                    desc="Switch to & move focused window to group {}".format(str(i)),
+                    i.name,
+                    lazy.window.togroup(i.name, switch_group=True),
+                    desc="Switch to & move focused window to group {}".format(i.name),
                 ),
             ]
         )
         
     groups.append(ScratchPad(numerOfGroups, [        
-    DropDown("btop", "alacritty -e btop", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),    
-    DropDown("explorer", "krusader", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),   
-    DropDown("nitrogen", "nitrogen", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),     
-    DropDown("terminal", "alacritty", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False ),
-]))
+        DropDown("btop", "alacritty -e btop", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),    
+        DropDown("explorer", "krusader", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),   
+        DropDown("nitrogen", "nitrogen", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),     
+        DropDown("terminal", "alacritty", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False ),
+    ]))
