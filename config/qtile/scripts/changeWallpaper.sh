76963f6da2d45f1fd23debc7e3fcee91fb198932
8u3631984 <8u3631984@gmail.com>
2024-09-24 14:32:00 +0200

    reload qtile configuration

--- config/qtile/scripts/changeWallpaper.sh
+++ config/qtile/scripts/changeWallpaper.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# set a random wallpaper
 feh --bg-scale --randomize ~/git/my-wallpaper/art
 
-# Get current wallpaper 
+# get last set deskcop wallpaper
 wallpaper="$(cat "${HOME}/.fehbg" | awk -F "'" '{print $2}')"
 
-#Apply pywal color scheme to desktop
+# set pywal color scheme
 wal -i $wallpaper
+
+# reload the qtile config
+qtile cmd-obj -o cmd -f reload_config
