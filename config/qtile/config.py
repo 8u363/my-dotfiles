0444c32d46c00b97a3d83ff9d0281a3b6affffb1
Sebastian Büge <sebastian_buege@gmx.de>
2024-09-25 09:55:21 +0200

    2 add feault qtile configuration (#10)
    * add qtile configuration
    * add widgets
    * add scripts to chnage wallpaper and powermenu
    * autostrat script

---
+++ config/qtile/config.py
@@ -0,0 +1,125 @@
+
+#   ___ _____ ___ _     _____    ____             __ _       
+#  / _ \_   _|_ _| |   | ____|  / ___|___  _ __  / _(_) __ _ 
+# | | | || |  | || |   |  _|   | |   / _ \| '_ \| |_| |/ _` |
+# | |_| || |  | || |___| |___  | |__| (_) | | | |  _| | (_| |
+#  \__\_\|_| |___|_____|_____|  \____\___/|_| |_|_| |_|\__, |
+#                                                      |___/ 
+
+# --------------------------------------------------------
+# see https://docs.qtile.org/
+# --------------------------------------------------------
+
+import os
+import subprocess
+import json
+from pathlib import Path
+
+from libqtile import bar, layout, qtile, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown
+from libqtile.lazy import lazy
+
+from qtile_extras import widget
+from qtile_extras.widget.decorations import PowerLineDecoration
+
+
+from globalVariables import *
+from configKeys import initKeys
+from configGroups import initGroups
+from configWidgets import initWidgets
+from configLayouts import initLayouts
+
+# --------------------------------------------------------
+# autostart configuration
+# --------------------------------------------------------
+@ hook.subscribe.startup_once
+def autostart():
+    autostartPath = os.path.expanduser(home + '/.config/qtile/autostart.sh')
+    subprocess.run([autostartPath])
+
+# --------------------------------------------------------
+# gernal configuration
+# --------------------------------------------------------
+dgroups_key_binder          = None
+dgroups_app_rules           = []  # type: list
+follow_mouse_focus          = True
+bring_front_click           = False
+floats_kept_above           = True
+cursor_warp                 = False
+
+auto_fullscreen             = True
+focus_on_window_activation  = "smart"
+reconfigure_screens         = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize               = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules              = None
+
+# xcursor theme (string or None) and size (integer) for Wayland backend
+wl_xcursor_theme            = None
+wl_xcursor_size             = 24
+
+wmname = "QTILE"
+
+# --------------------------------------------------------
+# key configuration
+# --------------------------------------------------------
+initKeys()
+
+# --------------------------------------------------------
+# group configuration
+# --------------------------------------------------------
+initGroups()
+
+# --------------------------------------------------------
+# widget configuration
+# --------------------------------------------------------
+initWidgets()
+
+# --------------------------------------------------------
+# layout configuration
+# --------------------------------------------------------
+initLayouts()
+
+# --------------------------------------------------------
+# screen configuration
+# --------------------------------------------------------
+screens = [
+    Screen(
+        top=bar.Bar(
+            widget_list,
+            40,
+            padding=20,
+            opacity=0.7,
+            border_width=[0, 0, 0, 0],
+            margin=[0,0,0,0],
+            background="#000000.3"
+        ),
+    ),
+]
+
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+
+# --------------------------------------------------------
+# mouse configuration
+# --------------------------------------------------------
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
