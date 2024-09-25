54ae22271ed36d018b2ffb04f9d72f3d907d500f
8u3631984 <8u3631984@gmail.com>
2024-09-25 09:32:05 +0200

    chnage output

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -2,7 +2,7 @@
 
 ~/.config/qtile/scripts/changeWallpaper.sh
 
-xrandr --output Virtual-1 --primary --mode 1600x1200 --pos 0x0 --rotate normal
+xrandr --output Virtual1 --primary --mode 1600x1200 --pos 0x0 --rotate normal
 
 dunst &
 blueman-applet &
