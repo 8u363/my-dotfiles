9f500c548f5b77459634f9e38821b80c3b7f7dfb
8u3631984 <8u3631984@gmail.com>
2024-10-19 21:57:33 +0200

    add group configuration

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -12,7 +12,6 @@
 
 import os
 import subprocess
-import json
 from pathlib import Path
 
 from libqtile import bar, layout, qtile, hook
@@ -25,14 +24,24 @@ from qtile_extras.widget.decorations import PowerLineDecoration
 
 from globalVariables import *
 from configKeys import initKeys
-from configGroups import initGroups
+#from configGroups import initGroups
 from configWidgets import initWidgets
 from configLayouts import initLayouts
+
+keys            = []   
+groups          = [Group(i) for i in "123456"]
+widget_list     = []
+layouts         = []
+
+home            = str(Path.home())  # get home path
+mod             = "mod4"            # windows key
+terminal        = "alacritty"
+numerOfGroups   = 6
 
 # --------------------------------------------------------
 # autostart configuration
 # --------------------------------------------------------
-@ hook.subscribe.startup_once
+@hook.subscribe.startup_once
 def autostart():
     autostartPath = os.path.expanduser(home + '/.config/qtile/autostart.sh')
     subprocess.run([autostartPath])
@@ -41,27 +50,18 @@ def autostart():
 # gernal configuration
 # --------------------------------------------------------
 dgroups_key_binder          = None
-dgroups_app_rules           = []  # type: list
+dgroups_app_rules           = []
 follow_mouse_focus          = True
 bring_front_click           = False
 floats_kept_above           = True
 cursor_warp                 = False
-
 auto_fullscreen             = True
 focus_on_window_activation  = "smart"
 reconfigure_screens         = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize               = True
-
-# When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules              = None
-
-# xcursor theme (string or None) and size (integer) for Wayland backend
 wl_xcursor_theme            = None
 wl_xcursor_size             = 24
-
 wmname = "QTILE"
 
 # --------------------------------------------------------
@@ -72,7 +72,42 @@ initKeys()
 # --------------------------------------------------------
 # group configuration
 # --------------------------------------------------------
-initGroups()
+for vt in range(1, numerOfGroups):
+    keys.append(
+        Key(
+            ["control", "mod1"],
+            f"f{vt}",
+            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+            desc=f"Switch to VT{vt}",
+        )
+    )
+
+for i in groups:
+    keys.extend(
+        [
+            # mod + group number = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod + shift + group number = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+        ]
+    )
+    
+groups.append(ScratchPad("6", [        
+    DropDown("btop", "alacritty -e btop", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),    
+    DropDown("explorer", "krusader", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),   
+    DropDown("nitrogen", "nitrogen", x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False  ),     
+    DropDown("terminal", terminal , x=0.1, y=0.1, width=0.80, height=0.80, on_focus_lost_hide=False ),
+]))
 
 # --------------------------------------------------------
 # widget configuration
