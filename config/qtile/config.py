874c216b263baf20204fe0f7f55559a4b2d7db8f
Sebastian Büge <sebastian_buege@gmx.de>
2024-10-28 11:05:11 +0100

    20 update qtile keybindings (#26)
    * update keybinding
    
    * update keybinding
    
    * update readme
    
    ---------
    
    Co-authored-by: 8u3631984 <8u3631984@gmail.com>

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -129,45 +129,42 @@ keys = [
     Key([mod], "q", lazy.window.kill(), desc="Close the focused window"),
     Key([mod, "shift"], "q", lazy.function(show_power_menu)),
 
+    Key([mod], "Space", lazy.spawn('betterlockscreen -l dim')),
+
     # Group functions
     Key([mod], "Tab", lazy.next_layout(), desc="Use next layout on the actual group"),
 
     # Layout function
-    Key([mod], "Left", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
 
-    Key([mod], "Right", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
 
-    Key([mod], "Down", lazy.layout.down(), desc="Move focus down"),
-    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
 
-    Key([mod], "Up", lazy.layout.up(), desc="Move focus up"),
-    Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
     # Window functions
-    Key([mod], "f", lazy.window.toggle_floating(), desc="Put the focused window to/from floating mode"),
-    Key([mod, "shift"], "f", lazy.window.toggle_fullscreen(), desc="Put the focused window to/from fullscreen mode"),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Put the focused window to/from fullscreen mode"),
+    Key([mod, "shift"], "f", lazy.window.toggle_floating(), desc="Put the focused window to/from floating mode"),
 
     # Apps
-    Key([mod], 'r', lazy.spawn('rofi -show run')),
-    Key([mod], "w", lazy.spawn("sh " + home + "/.config/qtile/scripts/changeWallpaper.sh")),
-    Key([mod], "b", lazy.spawn("brave"), desc="Launch browser"),
+    Key([mod], "r", lazy.spawn('rofi -show run')),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "w", lazy.spawn("sh " + home + "/.config/qtile/scripts/changeWallpaper.sh")),
 
     # scratch pad
     Key([mod], 'e', lazy.group["6"].dropdown_toggle("explorer")),
     Key([mod], "F2", lazy.group["6"].dropdown_toggle("terminal")),
-    Key([mod], "F3", lazy.group["6"].dropdown_toggle("nitrogen")),
-    Key([mod], 'F4', lazy.group["6"].dropdown_toggle("btop")),
+    Key([mod], 'F3', lazy.group["6"].dropdown_toggle("btop")),
 
     # Audio functions
     Key([], "XF86AudioLowerVolume", lazy.spawn("amixer sset Master 5%-"), desc="Lower Volume by 5%"),
     Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer sset Master 5%+"), desc="Raise Volume by 5%"),
     Key([], "XF86AudioMute", lazy.spawn("amixer sset Master 1+ toggle"), desc="Mute/Unmute Volume"),
-    Key([], "XF86AudioPlay", lazy.spawn("playerctl play-pause"), desc="Play/Pause player"),
-    Key([], "XF86AudioNext", lazy.spawn("playerctl next"), desc="Skip to next"),
-    Key([], "XF86AudioPrev", lazy.spawn("playerctl previous"), desc="Skip to previous"),
 ]
 
 # --------------------------------------------------------
